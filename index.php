ff7e1d028947f22d5df72bec90f49439311b1590
ErnaniUlsenheimer <ernaniu@gmail.com>
2021-09-01 11:45:27 -0300

    tela de login

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
+require_once("functions.php");
+
 use \Slim\Slim;
+use Hcode\Model\User;
 
 $app = new Slim();
 
@@ -18,10 +21,39 @@ $app->get('/', function() {
 
 $app->get('/admin', function() {
      
-
+    User::verifyLogin();
     $page = new Hcode\PageAdmin();
 
     $page->setTpl("index");  
+
+});
+
+$app->get('/admin/login', function() {
+    
+    $page = new Hcode\PageAdmin([
+        "header"=>false,
+        "footer"=>false
+    ]);
+
+    $page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function() {
+
+    User::login(post('deslogin'), post('despassword'));
+
+    header("Location: /admin");
+    exit;
+
+});
+
+$app->get('/admin/logout', function() {
+
+    User::logout();
+
+    header("Location: /admin/login");
+    exit;
 
 });
 
